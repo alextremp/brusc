8c4f2deec24df2971d2bf74771c049a92cd0e4e5
Alex Castells <alextremp@hotmail.com>
2019-12-27 05:32:35 +0100

    Develop/v1 (#2)
    * chore(config): update issues url
    
    * update project config
    
    * add auto-version from travis after github version tag

---
+++ version.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const shell = require('shelljs')
+
+const GIT_ORIGIN = `https://alextremp:${process.env.GH_TOKEN}@github.com/alextremp/brusc.git`
+
+const runCommand = function(command) {
+  shell.exec(command, function(code, stdout, stderr) {
+    console.log('>' + command)
+    console.log('Exit code:', code)
+    console.log('Output:', stdout, stderr)
+  })
+}
+
+const config = require('./package')
+const releaseVersion = process.env.TRAVIS_TAG.replace('v', '')
+const isBeta = releaseVersion.indexOf('-beta.') > -1
+config.version = releaseVersion
+
+fs.writeFileSync('package.json', JSON.stringify(config, null, 2))
+
+const commands = []
+const branch = isBeta ? `beta/${releaseVersion}` : 'master'
+commands.push('git config --global user.email "travis@travis-ci.org"')
+commands.push('git config --global user.name "Travis CI"')
+commands.push(`git checkout -b ${branch}`)
+commands.push(`git push --set-upstream origin ${branch}`)
+commands.push(`git remote rm origin`)
+commands.push(`git remote add origin ${GIT_ORIGIN} > /dev/null 2>&1`)
+commands.push('git add package.json')
+commands.push(`git commit -m "Update version to: ${releaseVersion}"`)
+commands.push(`git push origin ${branch} --quiet`)
+commands.push(`npm publish${isBeta ? ' --tag beta' : ''}`)
+
+commands.forEach(command => {
+  runCommand(command)
+})
