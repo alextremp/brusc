8051bbbb3e452ef0f15ab8f23372bb676ec9f8ee
Alex Castells <alextremp@hotmail.com>
2019-12-27 06:56:29 +0100

    test waiting commands

--- version.js
+++ version.js
@@ -8,7 +8,14 @@ const GIT_ORIGIN = `https://alextremp:${process.env.GH_TOKEN}@github.com/alextre
 const COMMIT_AUTHOR = 'Travis CI <travis@travis-ci.org>'
 const COMMIT_MESSAGE = `Update version to: ${releaseVersion}`
 
-const runCommand = function(command) {
+const commands = []
+
+const runCommand = function(i = 0) {
+  if (commands.length < i) {
+    console.log('Finished')
+    shell.exit(0)
+  }
+  const command = commands[i]
   shell.exec(command, function(code, stdout, stderr) {
     console.log('>' + command)
     console.log('> exit code', code)
@@ -17,6 +24,10 @@ const runCommand = function(command) {
       console.log('> ', stderr)
       shell.exit(code)
     }
+    const tid = setTimeout(function() {
+      clearTimeout(tid)
+      runCommand(i + 1)
+    }, 1000)
   })
 }
 
@@ -25,11 +36,11 @@ config.version = releaseVersion
 
 fs.writeFileSync('package.json', JSON.stringify(config, null, 2))
 
-const commands = []
 const branch = isBeta ? `beta/${releaseVersion}` : 'master'
 commands.push('git config --global user.email "alextremp@hotmail.com"')
 commands.push('git config --global user.name "Alex Castells"')
 commands.push(`git checkout -b ${branch}`)
+commands.push(`git push --repo=${GIT_ORIGIN} --set-upstream origin ${branch}`)
 commands.push(`git remote rm origin`)
 commands.push(`git remote add origin ${GIT_ORIGIN} > /dev/null 2>&1`)
 commands.push('git add package.json')
@@ -37,6 +48,4 @@ commands.push(`git commit -m "${COMMIT_MESSAGE}" --author="${COMMIT_AUTHOR}"`)
 commands.push(`git push --repo=${GIT_ORIGIN} origin ${branch} --quiet`)
 commands.push(`npm publish${isBeta ? ' --tag beta' : ''}`)
 
-commands.forEach(command => {
-  runCommand(command)
-})
+runCommand()
