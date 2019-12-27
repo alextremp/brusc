017798bedbe9df1509163df18b599f9ec8ca7f47
Alex Castells <alextremp@hotmail.com>
2019-12-28 00:06:09 +0100

    test version

--- version.js
+++ version.js
@@ -5,7 +5,6 @@ const config = require('./package')
 const releaseVersion = process.env.TRAVIS_TAG.replace('v', '')
 
 const REPO = `https://${process.env.GH_TOKEN}@github.com/alextremp/brusc.git`
-const AUTHOR = 'Travis CI <travis@travis-ci.org>'
 const MESSAGE = `[skip travis] Update version to: ${releaseVersion}`
 
 const commands = []
@@ -27,7 +26,7 @@ const runCommand = function(i = 0) {
     const tid = setTimeout(function() {
       clearTimeout(tid)
       runCommand(i + 1)
-    }, 1000)
+    }, 100)
   })
 }
 
@@ -37,16 +36,16 @@ config.version = releaseVersion
 fs.writeFileSync('package.json', JSON.stringify(config, null, 2))
 
 const NOLOG = ' > /dev/null 2>&1'
-const branch = isBeta ? `beta/${releaseVersion}` : 'master'
-commands.push('git config --global user.email "alextremp@hotmail.com"')
-commands.push('git config --global user.name "Alex Castells"')
+const branch = isBeta
+  ? `develop/v${releaseVersion.replace(/\.[0-9]+\.[0-9]+-beta\.[0-9]+/, '')}`
+  : 'master'
 commands.push(`git checkout -b ${branch}`)
 commands.push(`git push --repo=${REPO} --set-upstream origin ${branch}${NOLOG}`)
 commands.push(`git remote rm origin`)
-commands.push(`git remote add origin ${REPO} > /dev/null 2>&1`)
+commands.push(`git remote add origin ${REPO}${NOLOG}`)
 commands.push('git add package.json')
-commands.push(`git commit -m "${MESSAGE}" --author="${AUTHOR}"${NOLOG}`)
+commands.push(`git commit -m "${MESSAGE}"${NOLOG}`)
+// commands.push(`npm publish${isBeta ? ' --tag beta' : ''}`)
 commands.push(`git push --repo=${REPO} origin ${branch} --quiet${NOLOG}`)
-commands.push(`npm publish${isBeta ? ' --tag beta' : ''}`)
 
 runCommand()
