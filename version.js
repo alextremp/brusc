80d7325715cfe9ddfcf84fb7f938ad4484761246
Alex Castells <alextremp@hotmail.com>
2019-12-27 05:50:51 +0100

    improve auto-version

--- version.js
+++ version.js
@@ -1,18 +1,25 @@
 const fs = require('fs')
 const shell = require('shelljs')
 
+const config = require('./package')
+const releaseVersion = process.env.TRAVIS_TAG.replace('v', '')
+
 const GIT_ORIGIN = `https://alextremp:${process.env.GH_TOKEN}@github.com/alextremp/brusc.git`
+const COMMIT_AUTHOR = 'Travis CI <travis@travis-ci.org>'
+const COMMIT_MESSAGE = `Update version to: ${releaseVersion}`
 
 const runCommand = function(command) {
   shell.exec(command, function(code, stdout, stderr) {
     console.log('>' + command)
-    console.log('Exit code:', code)
-    console.log('Output:', stdout, stderr)
+    console.log('> exit code', code)
+    console.log('> ', stdout)
+    if (code !== 0) {
+      console.log('> ', stderr)
+      shell.exit(code)
+    }
   })
 }
 
-const config = require('./package')
-const releaseVersion = process.env.TRAVIS_TAG.replace('v', '')
 const isBeta = releaseVersion.indexOf('-beta.') > -1
 config.version = releaseVersion
 
@@ -20,14 +27,12 @@ fs.writeFileSync('package.json', JSON.stringify(config, null, 2))
 
 const commands = []
 const branch = isBeta ? `beta/${releaseVersion}` : 'master'
-commands.push('git config --global user.email "travis@travis-ci.org"')
-commands.push('git config --global user.name "Travis CI"')
 commands.push(`git checkout -b ${branch}`)
 commands.push(`git push --set-upstream origin ${branch}`)
 commands.push(`git remote rm origin`)
 commands.push(`git remote add origin ${GIT_ORIGIN} > /dev/null 2>&1`)
 commands.push('git add package.json')
-commands.push(`git commit -m "Update version to: ${releaseVersion}"`)
+commands.push(`git commit -m "${COMMIT_MESSAGE}" --author=${COMMIT_AUTHOR}`)
 commands.push(`git push origin ${branch} --quiet`)
 commands.push(`npm publish${isBeta ? ' --tag beta' : ''}`)
 
